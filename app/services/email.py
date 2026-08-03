d6748bda8ab276548edad2c4c8330986372c7878
engh <engahmedaymen93@gmail.com>
2026-08-03 23:26:07 +0300

    feat(email): update email notifications

--- app/services/email.py
+++ app/services/email.py
@@ -29,6 +29,23 @@ async def send_notification_email(payload: NotificationPayload):
         subject = "Your Ticket Purchase Confirmation"
         body = f"Thank you for your purchase! Your ticket code is {ticket_uuid}. Access your QR code here: {qr_code_url}"
 
+    elif  payload.event_type == EventType.EVENT_CREATE_REMINDER:
+        subject = "New Event Has Been Created"
+        link = f"{BASE_URL}/api/events/{payload.event_uuid}"
+        body = f"New Event has been created {payload.event_title}. For more information click here {link}"
+
+    elif payload.event_type == EventType.EVENT_REMINDER:
+        subject = f"Reminder: {payload.event_title} is tomorrow!"
+        link = f"{BASE_URL}/api/events/{payload.event_uuid}"
+        body = f"Hello! Sir {payload.name}, Reminder that {payload.event_title} starts in 24 hours, for your ticket {payload.ticket_uuid}. Check event details here {link}"
+
+    elif payload.event_type == EventType.PROMO_REMAINING_TICKETS:
+        tier = payload.ticket_tier
+        qty = payload.remaining_tickets
+        subject = f"Limited Tickets Available: {payload.event_title}"
+        link = f"{BASE_URL}/api/events/{payload.event_uuid}"
+        body = f"Only {qty} {tier} tickets remaining for {payload.event_title}! Get yours here {link}"
+        
     message = EmailMessage()
     message["From"] = settings.EMAILS_FROM
     message["To"] = payload.email
